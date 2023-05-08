9aea42b15b5bc0cf17b19858eace86812bdb42f9
Nomnom <104179573+NomnomPlays@users.noreply.github.com>
2023-05-08 14:53:41 -0500

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,4 +1,4 @@
-var sheetLocation = "https://cdn.jsdelivr.net/gh/NomnomPlays/CSS-Editor-Bookmarklets/";
+var sheetLocation = "https://raw.githubusercontent.com/NomnomPlays/CSS-Editor-Bookmarklets/main/";
 
 if (window.location.href.startsWith('https://lyons204.instructure.com')) {
   var canvas = document.createElement('link');
