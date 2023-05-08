28660698399160ee287ecb291154dfabbf7ff9f2
Nomnom <104179573+NomnomPlays@users.noreply.github.com>
2023-05-08 13:56:02 -0500

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,9 +1,11 @@
 var sheetLocation = 'https://raw.githubusercontent.com/NomnomPlays/CSS-Editor-Bookmarklets/main/';
 
 if (window.location.href.startsWith('https://lyons204.instructure.com')) {
-  var canvas = document.createElement('link')
+  var canvas = document.createElement('link');
   canvas.href = sheetLocation + 'canvas.css';
   canvas.rel = 'stylesheet';
   canvas.type = 'text/css';
   document.body.appendChild(canvas);
+  
+  alert(canvas);
 }
