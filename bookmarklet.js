3a632cc6d062164f967dfb4181d4eb3d3942065d
Nomnom <104179573+NomnomPlays@users.noreply.github.com>
2023-05-08 13:35:12 -0500

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,8 @@
+var sheetLocation = 'https://github.com/NomnomPlays/CSS-Editor-Bookmarklets/';
+
 if (window.location.href.startsWith('https://lyons204.instructure.com')) {
   var canvas = document.createElement('link')
-  canvas.href = 'https://bookmarkletscripts.nolananderson.repl.co/canvas.css';
+  canvas.href = sheetLocation + 'canvas.css';
   canvas.rel = 'stylesheet';
   canvas.type = 'text/css';
   document.body.appendChild(canvas);
