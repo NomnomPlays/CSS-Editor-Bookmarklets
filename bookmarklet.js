a62115ba7240a26baf2931db1817e3ede3ed729a
Nomnom <104179573+NomnomPlays@users.noreply.github.com>
2023-05-08 14:45:55 -0500

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,7 +1,5 @@
 var sheetLocation = "https://cdn.jsdelivr.net/gh/NomnomPlays/CSS-Editor-Bookmarklets/";
 
-alert("hi");
-
 if (window.location.href.startsWith('https://lyons204.instructure.com')) {
   var canvas = document.createElement('link');
   canvas.href = sheetLocation + 'canvas.css';
@@ -9,5 +7,5 @@ if (window.location.href.startsWith('https://lyons204.instructure.com')) {
   canvas.type = 'text/css';
   document.body.appendChild(canvas);
   
-  alert(sheetLocation + 'canvas.css');
+  alert(sheetLocation);
 }
